66db401ae02d40302f716e00e47b7fd2ac6c6297
arvind9990 <arvind.mern@gmail.com>
2026-05-09 07:42:28 +0530

    fix: add .jsx extension to PageNotFound import

--- frontend/src/components/routes/MyRoutes.jsx
+++ frontend/src/components/routes/MyRoutes.jsx
@@ -3,7 +3,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "../../pages/home/Home.jsx";
 import Signin from "../../pages/sign-in/Signin.jsx";
 import Signup from "../../pages/sign-up/Signup.jsx";
-import PageNotFound from "../../pages/page-not-found/PageNotFound"; // ✅ FIXED
+import PageNotFound from "../../pages/page-not-found/PageNotFound.jsx";
 import Profile from "../../pages/profile/Profile.jsx";
 import UpdateProfile from "../../pages/profile/UpdateProfile.jsx";
 
@@ -11,23 +11,19 @@ function MyRoutes({ isLoggedIn }) {
   return (
     <Routes>
       <Route path={"/"} element={isLoggedIn ? <Home /> : <Signin />} />
-
       <Route
         path={"/profile"}
         element={isLoggedIn ? <Profile /> : <Signin />}
       />
-
       <Route
         path={"/update-profile"}
         element={isLoggedIn ? <UpdateProfile /> : <Signin />}
       />
-
       <Route
         path={"/sign-up"}
         element={isLoggedIn ? <Navigate to="/" /> : <Signup />}
       />
-
-      <Route path="*" element={<PageNotFound />} /> {/* ✅ FIXED */}
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
